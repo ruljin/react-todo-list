42813fdc52f28b5c5a8ce98c26ba8f3c202ec84d
fkuca <fkuca91@gmail.com>
2022-02-05 20:08:07 +0100

    feat(create-todo): add container logic

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
+import { useState } from 'react';
 import { Header } from './components';
 import { CreateTodo } from './containers';
 import './app.css';
 
-const App = () => (
-	<div className='page'>
-		<Header text='React Todo List' />
-		<CreateTodo />
-	</div>
-);
+const App = () => {
+	const [list, setList] = useState([]);
+
+	const handleAddTodo = (todo) => setList([...list, todo]);
+
+	return (
+		<div className='page'>
+			<Header text='React Todo List' />
+			<CreateTodo handleAddTodo={handleAddTodo} />
+		</div>
+	);
+};
 
 export default App;
--- src/components/index.js
+++ src/components/index.js
@@ -1,1 +1,3 @@
 export { Header } from './header/Header';
+export { Input } from './input/Input';
+export { Button } from './button/Button';
